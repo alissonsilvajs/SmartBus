78ee676b8cb86409dc54e81726d07676c567422a
Alisson Silva <40868438+alissonsilvajs@users.noreply.github.com>
2019-11-20 14:43:11 -0300

    Update Program.ino

--- Program.ino
+++ Program.ino
@@ -1,22 +1,37 @@
+/*
+Project: A project for improve the urban mobility in the bus
+Arduino Mega, MFRC 522 and TM1637 Display
+Created: 11/21/2019
+Email: alissonsilva.py@gmail.com
+by Alisson Silva
+*/
+
+// Import the necessary libraries
 #include <SPI.h> 
 #include <MFRC522.h>
 #include <TM1637Display.h> 
 
+// Define the pins of Display
 #define CLK 2
 #define DIO 3
 #define TEST_DELAY 2000
 
+// Define the pins of MRFC 522
 #define SS_PIN 53
 #define RST_PIN 5
 
+#define tcrt5000 4
+
+// Create the instances for sensors
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 TM1637Display display(CLK, DIO);
 
 int contador = 0;
 bool deteccao;
 
-int tcrt5000 = 4; 
- 
+/*
+Configure the sensors
+*/
 void setup() 
 {
   SPI.begin();    
